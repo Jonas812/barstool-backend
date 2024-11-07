830ad98a4b610362bf042a5f39231ab156d3f0d3
Jonas <jonasneu812@gmail.com>
2024-11-07 19:17:50 +0100

    save

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllLocations(): string {
+    return 'all Locations!';
+  }
+
+  getLocationById(id: string): string {
+    return 'Location: ' + id;
   }
 }
